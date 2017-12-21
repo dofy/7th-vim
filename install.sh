92e3e4fe45b7873e357e9f86c07b14cea84d10c2
Seven Yu <dofyyu@gmail.com>
2017-12-21 15:08:24 +0800

    update install script

--- install.sh
+++ install.sh
@@ -15,7 +15,14 @@ die() {
 #[ -e "~/.vim" ] && die "~/.vim already exists."
 #[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
+# download .vimrc file
+curl -fLo ~/.vimrc \
+  https://raw.githubusercontent.com/dofy/7thvim/master/.vimrc
+# download .vimrc.local file
+curl -fLo ~/.vimrc.local \
+  https://raw.githubusercontent.com/dofy/7thvim/master/.vimrc.local
+# download vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qal
